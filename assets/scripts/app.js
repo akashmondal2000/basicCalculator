f83effa71e7404d0917362a6297ade4df5abc307
akashmondal2000 <mondalakash494@gmail.com>
2024-03-19 02:19:48 +0530

    add a function to log objects and fix import scripts

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,6 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
-let logEntries = []
+let logEntries = [];
 
 function userInputNumber() {
   return parseInt(userInput.value);
@@ -11,39 +11,55 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
   outputResult(currentResult, calcDescription);
 }
 
+function writeToLog(
+  operationIdentifire,
+  prevNumber,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIdentifire,
+    number: operationNumber,
+    previousNumber: prevNumber,
+    result: newResult,
+  }; // Create an object for recored of each entry
+
+  logEntries.push(logEntry);
+  console.log("User Input Entries =>", logEntries);
+}
+
 function add() {
   const enteredUserInput = userInputNumber();
   const initialResult = currentResult;
   currentResult = currentResult + enteredUserInput;
   createAndWriteOutput("+", initialResult, enteredUserInput);
-  logEntries.push(currentResult);
-  console.log("User Input Entries =>", logEntries);
+  writeToLog("Add",initialResult,enteredUserInput,currentResult);
 }
 
 function subtract() {
   const enteredUserInput = userInputNumber();
   const initialResult = currentResult;
   currentResult = currentResult - enteredUserInput;
-  createAndWriteOutput("-",initialResult,enteredUserInput);
-  logEntries.push(currentResult);
-  console.log("User Input Entries =>", logEntries)
+  createAndWriteOutput("-", initialResult, enteredUserInput);
+
+  writeToLog("Subtraction",initialResult,enteredUserInput,currentResult);
 }
 
 function multiplication() {
   const enteredUserInput = userInputNumber();
-  const initialResult = currentResult
+  const initialResult = currentResult;
   currentResult = currentResult * enteredUserInput;
-  createAndWriteOutput("*",initialResult,enteredUserInput);
-  logEntries.push(currentResult);
-  console.log("User Input Entries =>", logEntries);
+  createAndWriteOutput("*", initialResult, enteredUserInput);
+
+  writeToLog("Multiplication",initialResult,enteredUserInput,currentResult);
 }
 function division() {
   const enteredUserInput = userInputNumber();
   const initialResult = currentResult;
   currentResult = currentResult / enteredUserInput;
-  createAndWriteOutput("/",initialResult,enteredUserInput)
-  logEntries.push(currentResult);
-  console.log("User Input Entries =>", logEntries);
+  createAndWriteOutput("/", initialResult, enteredUserInput);
+
+  writeToLog("Division",initialResult,enteredUserInput,currentResult);
 }
 
 addBtn.addEventListener("click", add);
